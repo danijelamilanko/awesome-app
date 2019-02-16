5f24f76581b6fc2f1e70b63bc14c8848222de84e
Danijela Milanko <ddmilanko@gmail.com>
2019-02-16 21:40:26 +0100

    ready

--- client.js
+++ client.js
@@ -4,14 +4,12 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
 
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors());
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));

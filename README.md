b2ed0668a65beac9f79df6579c367bd92b454066
Danijela Milanko <ddmilanko@gmail.com>
2019-02-17 07:32:58 +0100

    fixes

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# AwesomeApp
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.4.
-
 ## Demo
 
 https://awesome-front.herokuapp.com
+
+# AwesomeApp
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.4.
 
 ## Development server
 
--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -3,14 +3,14 @@ import { User } from '../models/user/user';
 
 @Pipe({name: 'Filter'})
 export class FilterPipe implements PipeTransform {
-  transform(users: User[], query: string): any {
+  transform(userList: User[], query: string): any {
     if (query) {
-      return users.filter(user => (user.name.first.toLowerCase().indexOf(query.toLowerCase()) > -1 ||
+      return userList.filter(user => (user.name.first.toLowerCase().indexOf(query.toLowerCase()) > -1 ||
         user.name.last.toLowerCase().indexOf(query.toLowerCase()) > -1 ||
         user.email.toLowerCase().indexOf(query.toLowerCase()) > -1 ||
         user.dob.age.toString().toLowerCase().indexOf(query.toLowerCase()) > -1 ||
         user.phone.toLowerCase().indexOf(query.toLowerCase()) > -1));
     }
-    return users;
+    return userList;
   }
 }
